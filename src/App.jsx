2328b90e85e225e8ccc014f633dd8765587710f8
Giov3888 <giovannimahasano@gmail.com>
2025-10-07 19:15:02 +0300

    Nav bar create

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import './index.css'
-import {motion }  from 'framer-motion'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import Navbar from './components/TGS/NavBar';
 
 function App() {
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-      <h1 class="text-3xl font-bold underline text-black">
-        Hello world!
-      </h1>
-      <div className="mt-4 p-4 bg-white rounded shadow">
-        <motion.button className="btn btn-primary"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          Daisy UI Button
-        </motion.button>
-      </div>
+    <div className='flex flex-col items-center min-h-screen bg-gray-100'>
+      <Navbar />
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={< />} /> */}
+        </Routes>
+      </Router>
     </div>
   )
 }
